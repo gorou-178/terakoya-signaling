484b34567c766694896de3a111c62408296f616a
gurimmer <s.reianai@gmail.com>
2015-04-09 21:00:38 +0900

    use 'process.env.PORT' and port.

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 /**
  * Created by gurimmer on 2014/10/13.
  */
-var port = 5000;
-var io = require('socket.io').listen(port);
+var io = require('socket.io').listen(process.env.PORT || port);
 console.log((new Date()) + " Server is listening on port " + port);
 
 io.sockets.on('connection', function(socket) {
